92cd6cd8a439012a69f509c130deb458dbc7c1c1
Pierre MINIGGIO <=>
2020-03-02 11:09:23 +0100

    fix element w/ no class

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const doesSomeParentHaveClass = (element, classname) => {
-    if (element.className.split(' ').indexOf(classname)>=0) {
+const doesSomeParentHaveClass = (element, classname, checkCurrent = false) => {
+    if (
+        checkCurrent
+        && element.className !== undefined
+        && element.className.split(' ').indexOf(classname) >=0
+    ) {
 		return true
 	}
-    return element.parentNode && doesSomeParentHaveClass(element.parentNode, classname)
+    return element.parentNode && doesSomeParentHaveClass(element.parentNode, classname, true)
 }
 
 module.exports = doesSomeParentHaveClass
